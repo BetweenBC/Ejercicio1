bf7e651fa324ccf7ef2fdac2a46bdec6e98b05e3
BetweenBC <betweenbinarycode@gmail.com>
2017-07-26 09:46:22 +0200

    final

---
+++ src/Ejercicio.java
@@ -0,0 +1,28 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+/**
+ * Created by jsilva on 26/07/17.
+ */
+public class Ejercicio {
+
+    public static void main(String[] args) {
+
+
+        int num1 = 6;
+        int num2 = 3;
+        int miNumero = 4;
+
+        if(miNumero < num1){
+
+
+            System.out.println("Mi numero es menor que el el numero 1");
+
+        }
+
+    //Se puede cambiar la condicion del if
+
+
+
+    }
+}
